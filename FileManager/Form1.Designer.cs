0b5e12091c3867c2b25e32a053dd04cff91b7956
Vaylex <damirnurgaleeff@gmail.com>
2023-04-06 17:40:25 +0400

    file manager

---
+++ FileManager/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backButton = new System.Windows.Forms.Button();
+            this.goButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FileTypeLabel = new System.Windows.Forms.Label();
+            this.iconList = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(12, 12);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 0;
+            this.backButton.Text = "Назад";
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(713, 12);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "Вперед";
+            this.goButton.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.iconList;
+            this.listView1.Location = new System.Drawing.Point(15, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 376);
+            this.listView1.SmallImageList = this.iconList;
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(93, 12);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(614, 22);
+            this.filePathTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Имя файла";
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(90, 428);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(15, 13);
+            this.fileNameLabel.TabIndex = 5;
+            this.fileNameLabel.Text = "--";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(689, 428);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Тип файла";
+            // 
+            // FileTypeLabel
+            // 
+            this.FileTypeLabel.AutoSize = true;
+            this.FileTypeLabel.Location = new System.Drawing.Point(773, 428);
+            this.FileTypeLabel.Name = "FileTypeLabel";
+            this.FileTypeLabel.Size = new System.Drawing.Size(15, 13);
+            this.FileTypeLabel.TabIndex = 7;
+            this.FileTypeLabel.Text = "--";
+            // 
+            // iconList
+            // 
+            this.iconList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.iconList.ImageSize = new System.Drawing.Size(48, 48);
+            this.iconList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.Controls.Add(this.FileTypeLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.backButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label FileTypeLabel;
+        private System.Windows.Forms.ImageList iconList;
+    }
+}
+
---
+++ FileManager/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    } 4:16 видос https://www.youtube.com/watch?v=oRtME1oJD7M&t=256s&ab_channel=SnatchDreams
+}
